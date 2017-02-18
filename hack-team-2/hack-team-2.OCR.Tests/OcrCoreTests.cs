4f26d72223a1aa2804de1e240d664506bf887d0a
ovidiu-porumb <ovidiu.porumb@live.com>
2017-02-18 17:56:11 +0200

    added the ocrcore library

---
+++ hack-team-2/hack-team-2.OCR.Tests/OcrCoreTests.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using NUnit.Framework;
+// ReSharper disable InconsistentNaming
+
+namespace hack_team_2.OCR.Tests
+{
+    [TestFixture]
+    public class OcrCoreTests
+    {
+        private string subscriptionKey;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            subscriptionKey = File.ReadAllText("Data\\TestSubscriptionKey.txt");
+        }
+        [Test]
+        public async void Given_an_image_with_text_the_text_is_extracted()
+        {
+            Stream image = new FileStream("Data\\test.jpg", FileMode.Open);
+
+            var ocrCore = new OcrCore(subscriptionKey);
+            string actual = await ocrCore.ExtractTextFromImage(image, "unk");
+
+            string expected = File.ReadAllText("Data\\testExpected.txt");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ hack-team-2/hack-team-2.OCR/OcrCore.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+
+// ReSharper disable InconsistentNaming
+
+namespace hack_team_2.OCR
+{
+    public class OcrCore
+    {
+        private readonly string subscriptionKey;
+
+        public OcrCore(string subscriptionKey)
+        {
+            this.subscriptionKey = subscriptionKey;
+        }
+
+        public async Task<string> ExtractTextFromImage(Stream imageStream, string language)
+        {
+            var visionServiceClient = new VisionServiceClient(subscriptionKey);
+
+            OcrResults ocrResult = await visionServiceClient.RecognizeTextAsync(imageStream, language);
+
+            string textResult = AggregateOcrResults(ocrResult);
+
+            return textResult;
+        }
+
+        private string AggregateOcrResults(OcrResults ocrResults)
+        {
+            var result = new StringBuilder();
+
+            if (ocrResults != null && ocrResults.Regions != null)
+            {
+                foreach (var item in ocrResults.Regions)
+                {
+                    foreach (var line in item.Lines)
+                    {
+                        foreach (var word in line.Words)
+                        {
+                            result.Append(word.Text);
+                            result.Append(" ");
+                        }
+                        result.AppendLine();
+                    }
+                    result.AppendLine();
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
