bfaf924c494f10264ca9fb0ed27999c17453d107
ovidiu-porumb <ovidiu.porumb@live.com>
2017-02-19 00:58:29 +0200

    extracted config data into web.config

--- hack-team-2/hack-team-2.sharepoint/DocumentOperation.cs
+++ hack-team-2/hack-team-2.sharepoint/DocumentOperation.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
-using System.Security;
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+using hack_team_2.OCR;
 using Microsoft.SharePoint.Client;
 using File = System.IO.File;
 
@@ -7,9 +10,58 @@ namespace hack_team_2.sharepoint
 {
     public class DocumentOperation
     {
-        public void UploadImageAsWordDocument(string imageFilePath)
+        public async void UploadImageAsWordDocument(string imageFilePath)
         {
-            
+            string wordDocumentFilePath = CreateWordDocument(imageFilePath);
+            string textFromImage = await ExtractTextFromImage(imageFilePath);
+
+            UploadDocumentToSharepoint(wordDocumentFilePath, textFromImage);
+        }
+
+        private void UploadDocumentToSharepoint(string wordDocumentFilePath, string textFromImage)
+        {
+            using (var context = new ClientContext(SharepointConnectionData.WebFullUrl))
+            {
+                context.Credentials = new SharePointOnlineCredentials(SharepointConnectionData.Username, SharepointConnectionData.Password);
+                var web = context.Web;
+                var newFile = new FileCreationInformation
+                {
+                    Content = File.ReadAllBytes(wordDocumentFilePath),
+                    Url = Path.GetFileName(wordDocumentFilePath)
+
+                };
+                var docs = web.Lists.GetByTitle("Documents");
+                Microsoft.SharePoint.Client.File doc = docs.RootFolder.Files.Add(newFile);
+                var item = doc.ListItemAllFields;
+                item["OCR"] = textFromImage;
+                item.Update();
+
+                context.ExecuteQuery();
+            }
+        }
+
+        private string CreateWordDocument(string imageFilePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<string> ExtractTextFromImage(string imageFilePath)
+        {
+            string subscriptionKey = LoadSubscriptionKey();
+            OcrCore ocrCore = new OcrCore(subscriptionKey);
+
+            using (Stream imageStream = new FileStream(imageFilePath, FileMode.Open))
+            {
+                string text = await ocrCore.ExtractTextFromImage(imageStream, "unk");
+                return text;
+            }
+
+            throw new Exception("exception thrown from ExtractTextFromImage");
+        }
+
+        private string LoadSubscriptionKey()
+        {
+            return ConfigurationManager.AppSettings["OcrSubscriptionKey"];
         }
     }
 }
---
+++ hack-team-2/hack-team-2.sharepoint/SharepointConnectionData.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using System.Security;
+
+namespace hack_team_2.sharepoint
+{
+    public class SharepointConnectionData
+    {
+        public static string WebFullUrl => ConfigurationManager.AppSettings["SharepointUrl"];
+        public static string Username => ConfigurationManager.AppSettings["Username"];
+        public static SecureString Password
+        {
+            get
+            {
+                var password = new SecureString();
+                foreach (var c in ConfigurationManager.AppSettings["SharepointUrl"])
+                {
+                    password.AppendChar(c);
+                }
+                return password;
+            }
+        }
+    }
+}
